8ffc266be838f016e782cc590f53e9ac5624b3f3
carmenfan <wind5320@hotmail.com>
2015-08-28 10:09:25 +0100

    ISSUE #7 don't bother broadcasting a message if it is empty/just have spaces or newline
    This happens with the piping of cerr as it is not buffered

--- src/repo/lib/repo_broadcaster.cpp
+++ src/repo/lib/repo_broadcaster.cpp
@@ -1,5 +1,5 @@
 #include "repo_broadcaster.h"
-
+#include <boost/algorithm/string.hpp>
 using namespace repo::lib;
 
 RepoBroadcaster::RepoBroadcaster()
@@ -22,6 +22,21 @@ RepoBroadcaster::~RepoBroadcaster()
 std::streamsize RepoBroadcaster::write(const char* s, std::streamsize n)
 {
 	std::string msg(s, n);
+
+	size_t firstPos = msg.find_first_of('%') + 1;
+	size_t secPos = msg.find_first_of('%', firstPos + 1);
+
+	std::string actualMessage = msg;
+	if (firstPos != std::string::npos && secPos != std::string::npos)
+	{
+		actualMessage = msg.substr(secPos + 1);
+
+	}
+	
+	boost::trim(actualMessage); //remove trailing spaces/carriage returns 
+
+
+	if (actualMessage.empty()) return n; //don't bother broadcasting if the actual message is empty
 
 	for (const auto &listener : subscribers)
 	{
