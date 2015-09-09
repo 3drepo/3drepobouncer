09189d2c3700271f5ddb0dbbf7322ac3b5fd1a9e
carmenfan <wind5320@hotmail.com>
2015-09-09 17:31:40 +0100

    ISSUE #13 simple test on repoBSON (not completed)

---
+++ test/unit/repo/core/model/bson/ut_repo_bson.cpp
@@ -0,0 +1,56 @@
+/**
+*  Copyright (C) 2015 3D Repo Ltd
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as
+*  published by the Free Software Foundation, either version 3 of the
+*  License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtest/gtest.h>
+
+#include <repo/core/model/bson/repo_bson.h>
+
+using namespace repo::core::model;
+
+static const RepoBSON testBson = RepoBSON(BSON("ice" << "lolly" << "amount" << 100));
+
+/**
+* Construct from mongo builder and mongo bson should give me the same bson
+*/
+TEST(RepoBSONTest, ConstructFromMongo)
+{
+	mongo::BSONObj mongoObj = BSON("ice" << "lolly" << "amount" << 100);
+	mongo::BSONObjBuilder builder;
+	builder << "ice" << "lolly";
+	builder << "amount" << 100;
+
+	RepoBSON bson1(mongoObj);
+	RepoBSON bson2(builder);
+	RepoBSON bsonDiff(BSON("something" << "different"));
+
+	EXPECT_EQ(bson1, bson2);
+	EXPECT_EQ(bson1.toString(), bson2.toString());
+	EXPECT_NE(bson1, bsonDiff);
+
+}
+
+/**
+* Test getfield
+*/
+TEST(RepoBSONTest, GetField)
+{
+	EXPECT_EQ(testBson.getField("ice"), testBson.getField("ice"));
+	EXPECT_NE(testBson.getField("ice"), testBson.getField("amount"));
+
+	EXPECT_EQ("lolly", testBson.getField("ice").str());
+	EXPECT_EQ(100, testBson.getField("amount").Int());
+}
