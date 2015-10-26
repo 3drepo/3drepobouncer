97afe5282058cc91a66d163e8fbfc6dcde34528d
Jozef Doboš <j.dobos@cs.ucl.ac.uk>
2015-10-26 17:53:49 +0000

    Updated win_build.cmd with post-build copies to c:\local\3drepobouncer

--- win_build.cmd
+++ win_build.cmd
@@ -8,6 +8,11 @@ python updateSources.py
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=C:/local/3drepobouncer -G "Visual Studio 12 Win64" ../
-msbuild ALL_BUILD.vcxproj /v:quiet /p:Configuration=Release /t:Rebuild 
+msbuild ALL_BUILD.vcxproj /v:quiet /p:Configuration=Release /t:Rebuild
+copy bouncer\Release\ c:\local\3drepobouncer\lib
+copy ..\bouncer\src\ c:\local\3drepobouncer\include
+ 
 msbuild ALL_BUILD.vcxproj /v:quiet /p:Configuration=Debug
+copy bouncer\Debug\ c:\local\3drepobouncer\lib
+
 cd ..
