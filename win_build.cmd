28c32722233bd242e42a1e4808e33f28e91839a6
Jozef Doboš <j.dobos@cs.ucl.ac.uk>
2015-10-26 18:09:53 +0000

    Updated win_build.cmd to INSTALL proj by carmenfan

--- win_build.cmd
+++ win_build.cmd
@@ -8,11 +8,6 @@ python updateSources.py
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=C:/local/3drepobouncer -G "Visual Studio 12 Win64" ../
-msbuild ALL_BUILD.vcxproj /v:quiet /p:Configuration=Release /t:Rebuild
-copy bouncer\Release\ c:\local\3drepobouncer\lib
-copy ..\bouncer\src\ c:\local\3drepobouncer\include
- 
-msbuild ALL_BUILD.vcxproj /v:quiet /p:Configuration=Debug
-copy bouncer\Debug\ c:\local\3drepobouncer\lib
-
+msbuild INSTALL.vcxproj /v:quiet /p:Configuration=Release /t:Rebuild
+msbuild INSTALL.vcxproj /v:quiet /p:Configuration=Debug
 cd ..
