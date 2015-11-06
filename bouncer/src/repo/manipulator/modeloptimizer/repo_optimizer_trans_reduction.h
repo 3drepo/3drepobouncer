89a972df84f02a48fdc7cf14d9117d45987d20e8
carmenfan <wind5320@hotmail.com>
2015-11-06 11:12:08 +0000

    #13 initial commit

---
+++ bouncer/src/repo/manipulator/modeloptimizer/repo_optimizer_trans_reduction.h
@@ -0,0 +1,53 @@
+/**
+*  Copyright (C) 2015 3D Repo Ltd
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as
+*  published by the Free Software Foundation, either version 3 of the
+*  License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* Transformation Reduction Optimizer
+* Reduces the amount of transformations within the scene graph by
+* merging transformation with its mesh (provided the mesh doesn't have
+* multiple parent
+*/
+
+#include "repo_optimizer_abstract.h"
+
+namespace repo {
+	namespace manipulator {
+		namespace modeloptimizer {
+			class TransformationReductionOptimizer : AbstractOptimizer{
+
+			public:
+				/**
+				* Default constructor
+				*/
+				TransformationReductionOptimizer();
+
+				/**
+				* Default deconstructor
+				*/
+				virtual ~TransformationReductionOptimizer();
+
+
+				/**
+				* Apply optimisation on the given repoScene
+				* @param scene takes in a repoScene to optimise
+				* @return returns true upon success
+				*/
+				virtual bool apply(repo::core::model::RepoScene *scene);
+			};
+		}
+	}
+}
