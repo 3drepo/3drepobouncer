cf0c16b96257b3142a3b9094f8ee6708c6c11f80
carmenfan <wind5320@hotmail.com>
2016-04-15 16:50:33 +0100

    #103 first attempt at selectiontree generation

---
+++ bouncer/src/repo/manipulator/modelutility/repo_maker_selection_tree.h
@@ -0,0 +1,73 @@
+/**
+*  Copyright (C) 2016 3D Repo Ltd
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as
+*  published by the Free Software Foundation, either version 3 of the
+*  License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include "../../core/model/collection/repo_scene.h"
+#include "../../lib/repo_property_tree.h"
+
+namespace repo{
+	namespace manipulator{
+		namespace modelutility{
+			//FIXME: remove export tag
+			class REPO_API_EXPORT SelectionTreeMaker
+			{
+			public:
+				/**
+				* Create a selection tree maker.
+				* The scene must be a valid pointer with Default graph loaded
+				* @params scene scene to construct from
+				*/
+				SelectionTreeMaker(
+					const repo::core::model::RepoScene *scene);
+				~SelectionTreeMaker();
+
+				/**
+				* Construct and return the selection tree as a Property Tree
+				* The method will return an empty tree if the scene is null
+				* or the default graph is not loaded.
+				* @return returns the selection tree as a property tree
+				*/
+				repo::lib::PropertyTree getSelectionTreeAsPropertyTree() const;
+
+				/**
+				* Construct and return the selection tree as a Property Tree As a buffer
+				* The method will return an empty tree if the scene is null
+				* or the default graph is not loaded.
+				* @return returns the selection tree as a property tree
+				*/
+				std::vector<uint8_t> getSelectionTreeAsBuffer() const;
+				
+			private:
+				const repo::core::model::RepoScene *scene;
+
+				/**
+				* Recurse function to generate property tree for a specific node
+				* @param currentNode node to parse
+				* @param idMap a map of pairs of id to name mapping (to insert)
+				* @param currentPath the current path that leads to this node.
+				*/
+				repo::lib::PropertyTree generatePTree(
+					const repo::core::model::RepoNode            *currentNode,
+					std::unordered_map<std::string,
+						   std::pair<std::string, std::string >> &idMaps,
+					const std::string                            &currentPath) const;
+
+			};
+
+
+		}
+	}
+}
