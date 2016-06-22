6ffd28a8220a41944e93cf3696e6d341420ea061
carmenfan <wind5320@hotmail.com>
2016-06-22 10:23:20 +0100

    #130 add RelVoid optimizer

--- bouncer/src/repo/manipulator/modeloptimizer/repo_optimizer_ifc.cpp
+++ bouncer/src/repo/manipulator/modeloptimizer/repo_optimizer_ifc.cpp
@@ -19,12 +19,17 @@
 * Abstract optimizer class
 */
 
+
 #include "repo_optimizer_ifc.h"
 #include "../../core/model/bson/repo_node_transformation.h"
 #include <algorithm>
 
 using namespace repo::manipulator::modeloptimizer;
 const static std::string IFC_MAPPED_ITEM = "IFCMAPPEDITEM";
+const static std::string IFC_RELVOID_ELE = "$RELVOIDSELEMENT";
+const static std::string IFC_RELVOID_ELE2 = "IFCRELVOIDSELEMENT";
+
+auto defaultG = repo::core::model::RepoScene::GraphType::DEFAULT;
 
 IFCOptimzer::IFCOptimzer()
 {
@@ -40,7 +45,7 @@ bool IFCOptimzer::apply(repo::core::model::RepoScene *scene)
 	std::unordered_map<std::string, std::vector<repo::core::model::RepoNode*>> transByName;
 	//sort transformations by name
 
-	auto tranNodes = scene->getAllTransformations(repo::core::model::RepoScene::GraphType::DEFAULT);
+	auto tranNodes = scene->getAllTransformations(defaultG);
 	for (auto &trans : tranNodes)
 	{
 		auto name = trans->getName();
@@ -53,7 +58,9 @@ bool IFCOptimzer::apply(repo::core::model::RepoScene *scene)
 		transByName[name].push_back(trans);
 	}
 
+	success = optimizeRelVoidElements(scene, transByName);
 	success = optimizeIFCMappedItems(scene, transByName);
+
 	return success;
 }
 
@@ -64,7 +71,7 @@ bool IFCOptimzer::optimizeIFCMappedItems(
 	bool success = true;
 	auto it = transByName.find(IFC_MAPPED_ITEM);
 	repoTrace << "Optimising " << IFC_MAPPED_ITEM << "s...";
-	auto defaultG = repo::core::model::RepoScene::GraphType::DEFAULT;
+
 	if (it != transByName.end())
 	{
 		//There must be a sibling that is a metadata, with nodes as child
@@ -111,11 +118,12 @@ bool IFCOptimzer::optimizeIFCMappedItems(
 					//reparent its children to its parent
 					//assign metadata to children
 					scene->abandonChild(defaultG, parent, mappedItemNode);
-					////FIXME: to remove
-					//for (auto &meta : metaVector)
-					//{
-					//	scene->abandonChild(defaultG, parent, meta->getSharedID());
-					//}
+#ifdef DEBUG_ME
+					for (auto &meta : metaVector)
+					{
+						scene->abandonChild(defaultG, parent, meta->getSharedID());
+					}
+#endif
 
 					for (auto &child : children)
 					{
@@ -145,4 +153,149 @@ bool IFCOptimzer::optimizeIFCMappedItems(
 	}
 
 	return success;
+}
+
+bool IFCOptimzer::optimizeRelVoidElements(
+	repo::core::model::RepoScene *scene,
+	const std::unordered_map<std::string, std::vector<repo::core::model::RepoNode*>>  &transByName)
+{
+	bool success = true;
+
+	auto it = transByName.find(IFC_RELVOID_ELE);
+	auto it2 = transByName.find(IFC_RELVOID_ELE2);
+
+	std::vector<repo::core::model::RepoNode*> relVoidArray;
+	if (it != transByName.end())
+	{
+		relVoidArray = it->second;
+	}
+
+	if (it2 != transByName.end())
+	{
+		//Shouldn't exist, but just incase.
+		relVoidArray.insert(relVoidArray.end(), it2->second.begin(), it2->second.end());
+	}
+
+	repoTrace << "Optimising " << IFC_RELVOID_ELE2 << "s...";
+
+	for (auto &voidNode : relVoidArray)
+	{
+		/*
+		* If Identity:
+		*		- If there is metadata within that subbranch, shrink up the RelVoids and maintain the sub branch
+		*		- If all there is is transformations within the subbranch, remove the whole branch
+		* If not identity:
+		*		- not expected to happen, but leave it as it is.
+		*/
+
+		auto transNode = dynamic_cast<repo::core::model::TransformationNode *>(voidNode);
+		if (transNode->isIdentity())
+		{
+			std::vector<repoUUID> ids;
+			auto parents = transNode->getParentIDs();
+
+			//Check if all of its parents have a meta node as child, if it does, append the metadata to the meshes
+			for (const auto &parent : parents)
+			{
+				auto metaVector = scene->getChildrenNodesFiltered(defaultG,
+					parent, repo::core::model::NodeType::METADATA);
+				auto meshVector = scene->getChildrenNodesFiltered(defaultG,
+					parent, repo::core::model::NodeType::MESH);
+
+				for (auto &mesh : meshVector)
+				{
+					for (auto &meta : metaVector)
+					{
+						scene->addInheritance(defaultG, mesh, meta);
+#ifdef DEBUG_ME
+						scene->abandonChild(defaultG, parent, meta);
+#endif
+					}
+				}
+
+				//remove linkage to parent
+				scene->abandonChild(defaultG, parent, transNode, true, false);
+			}
+
+			if (isBranchMeaningless(scene, transNode, ids))
+			{
+				//Branch is meaningless, deleting the whole thing
+				repoTrace << "Found meaningless " << IFC_RELVOID_ELE2 << " branch, removing...";
+				for (const auto &id : ids)
+					scene->removeNode(defaultG, id);
+			}
+			else
+			{
+				repoTrace << "Found " << IFC_RELVOID_ELE2 << " branch contains info, preserving...";
+				auto children = scene->getChildrenAsNodes(defaultG, transNode->getSharedID());
+
+				for (auto &child : children)
+				{
+					scene->abandonChild(defaultG, transNode->getSharedID(), child, false, true);
+				}
+
+				//remove itself and append child to parent
+				for (const auto &parent : parents)
+				{
+					auto parentNode = scene->getNodeBySharedID(defaultG, parent);
+					if (parentNode)
+					{
+						for (auto &child : children)
+						{
+							scene->addInheritance(defaultG, parentNode, child);
+						}
+					}
+				}
+
+				scene->removeNode(defaultG, transNode->getSharedID());
+			}
+		}
+	}
+
+	return success;
+}
+
+bool IFCOptimzer::isBranchMeaningless(
+	const repo::core::model::RepoScene *scene,
+	const repo::core::model::RepoNode  *node,
+	std::vector<repoUUID>              &ids,
+	const uint32_t                           &currentDepth,
+	const uint32_t                           &maxDepth)
+{
+	if (currentDepth == maxDepth)
+	{
+		repoTrace << "Hit max depth";
+		return false;
+	}
+
+	auto sharedID = node->getSharedID();
+	auto children = scene->getChildrenAsNodes(defaultG, sharedID);
+
+	ids.push_back(sharedID);
+	for (const auto &child : children)
+	{
+		if (child->getTypeAsEnum() == repo::core::model::NodeType::TRANSFORMATION)
+		{
+			auto transChild = dynamic_cast<repo::core::model::TransformationNode*>(child);
+			if (!isBranchMeaningless(scene, child, ids, currentDepth + 1, maxDepth))
+			{
+				repoTrace << "Child is meaningful";
+				return false;
+			}
+
+			//ensure this transformation has no other parent
+			if (transChild->getParentIDs().size() > 1)
+			{
+				repoTrace << "Child has more than one parent";
+				return false;
+			}
+		}
+		else
+		{
+			repoTrace << "Child[" << child->getUniqueID() << "] is not transformation";
+			return false;
+		}
+	}
+
+	return true;
 }
--- bouncer/src/repo/manipulator/modeloptimizer/repo_optimizer_ifc.h
+++ bouncer/src/repo/manipulator/modeloptimizer/repo_optimizer_ifc.h
@@ -49,13 +49,41 @@ namespace repo {
 
 			private:
 				/**
+				* Determine if the branch has no useful information in it
+				* @param scene the scene in question
+				* @param node root node of this sub branch
+				* @param ids ids within this subBranch (not exhaustive if false is returned
+				* @param currentDepth current depth of this recursive search
+				* @param maxDepth maximum depth before giving up
+				* @return returns true if all it contains is transformations, false otherwise or given up
+				*/
+				bool isBranchMeaningless(
+					const repo::core::model::RepoScene *scene,
+					const repo::core::model::RepoNode  *node,
+					std::vector<repoUUID>              &ids,
+					const uint32_t                           &currentDepth = 0,
+					const uint32_t                           &maxDepth = 2);
+
+				/**
 				* Optimize away the IFC Mapped Items.
-				* It is a IFC keyword to group meshes with same metadata
+				* It is an IFC keyword to group meshes with same metadata
 				* @param scene takes in a repoScene to optimise
 				* @param transByName mapping of transformation and their name (capitalized)
 				* @return returns true upon success
 				*/
-				bool IFCOptimzer::optimizeIFCMappedItems(
+				bool optimizeIFCMappedItems(
+					repo::core::model::RepoScene *scene,
+					const std::unordered_map<std::string, std::vector<repo::core::model::RepoNode*>>  &transByName);
+
+				/**
+				* Optimize away the IfcRelVoidsElement.
+				* It is an IFC Keyword to identify a hole in a geometry, which
+				* is redundant after assimp processing and holds no information
+				* @param scene takes in a repoScene to optimise
+				* @param transByName mapping of transformation and their name (capitalized)
+				* @return returns true upon success
+				*/
+				bool optimizeRelVoidElements(
 					repo::core::model::RepoScene *scene,
 					const std::unordered_map<std::string, std::vector<repo::core::model::RepoNode*>>  &transByName);
 			};
