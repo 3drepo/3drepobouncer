bf20badf8186c7c375dd2f8c873ed279203b7301
carmenfan <wind5320@hotmail.com>
2016-06-24 17:46:52 +0100

    #13 texture node done

--- bouncer/src/repo/core/model/bson/repo_node_texture.h
+++ bouncer/src/repo/core/model/bson/repo_node_texture.h
@@ -56,6 +56,24 @@ namespace repo {
 				~TextureNode();
 
 				/**
+				* Get the type of node
+				* @return returns the type as a string
+				*/
+				virtual std::string getType() const
+				{
+					return REPO_NODE_TYPE_TEXTURE;
+				}
+
+				/**
+				* Get the type of node as an enum
+				* @return returns type as enum.
+				*/
+				virtual NodeType getTypeAsEnum() const
+				{
+					return NodeType::TEXTURE;
+				}
+
+				/**
 				* Check if the node is semantically equal to another
 				* Different node should have a different interpretation of what
 				* this means.
