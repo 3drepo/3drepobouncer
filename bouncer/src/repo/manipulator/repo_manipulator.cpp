dfc545947663e2ad15a76250a7b4b0bad8c2620c
carmenfan <wind5320@hotmail.com>
2016-06-24 18:22:02 +0100

    #13 remove semi colon

--- bouncer/src/repo/manipulator/repo_manipulator.cpp
+++ bouncer/src/repo/manipulator/repo_manipulator.cpp
@@ -32,7 +32,7 @@
 #include "modelconvertor/export/repo_model_export_assimp.h"
 #include "modelconvertor/import/repo_metadata_import_csv.h"
 #include "modeloptimizer/repo_optimizer_trans_reduction.h"
-#include "modeloptimizer/repo_optimizer_ifc.h";
+#include "modeloptimizer/repo_optimizer_ifc.h"
 #include "modelutility/repo_scene_cleaner.h"
 #include "modelutility/repo_scene_manager.h"
 #include "modelutility/spatialpartitioning/repo_spatial_partitioner_rdtree.h"
