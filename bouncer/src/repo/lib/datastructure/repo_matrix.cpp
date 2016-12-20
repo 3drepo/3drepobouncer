826868a59bb480bf9722d08f63a8bcc650969c0f
carmenfan <wind5320@hotmail.com>
2016-12-20 12:49:00 +0000

    #48 include cmath for sqrt

--- bouncer/src/repo/lib/datastructure/repo_matrix.cpp
+++ bouncer/src/repo/lib/datastructure/repo_matrix.cpp
@@ -15,7 +15,6 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
 
 #include "repo_matrix.h"
 #include <sstream>
--- bouncer/src/repo/lib/datastructure/repo_vector3d.h
+++ bouncer/src/repo/lib/datastructure/repo_vector3d.h
@@ -21,6 +21,7 @@
 
 #include <vector>
 #include <sstream>
+#include <cmath>
 
 namespace repo{
 	namespace lib{
