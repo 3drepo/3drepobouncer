5b9cb8a2896432d89f3470bc8d6ad1c6fe993fb2
carmenfan <wind5320@hotmail.com>
2018-01-24 10:55:50 +0000

    ISSUE #231 fix bug where an unintialised variable is used

--- bouncer/src/repo/manipulator/modelutility/repo_maker_selection_tree.cpp
+++ bouncer/src/repo/manipulator/modelutility/repo_maker_selection_tree.cpp
@@ -125,8 +125,10 @@ repo::lib::PropertyTree SelectionTreeMaker::generatePTree(
 			hiddenOnDefault = true;
 			hiddenNode.push_back(idString);
 		}
-		else if (hiddenOnDefault = hiddenOnDefault || hasHiddenChildren)
+		else if (hiddenOnDefault || hasHiddenChildren)
 		{
+			hiddenOnDefault = (hiddenOnDefault || hasHiddenChildren);
+			repoDebug << "Setting " << name << " to half hidden... hiddenOnDefault: " << hiddenOnDefault << " hasHiddenChildren: " << hasHiddenChildren;
 			tree.addToTree(REPO_LABEL_VISIBILITY_STATE, REPO_VISIBILITY_STATE_HALF_HIDDEN);
 		}
 		else
@@ -186,7 +188,7 @@ std::map<std::string, repo::lib::PropertyTree>  SelectionTreeMaker::getSelection
 	{
 		std::unordered_map< std::string, std::pair<std::string, std::string>> map;
 		std::vector<std::string> hiddenNodes, childrenMeshes;
-		bool dummy;
+		bool dummy = false;
 		repo::lib::PropertyTree tree, settingsTree, treePathTree, shareIDToUniqueIDMap, idToMeshes;
 		std::vector<std::pair<std::string, std::string>>  sharedIDToUniqueID; 
 		tree.mergeSubTree("nodes", generatePTree(root, map, sharedIDToUniqueID, idToMeshes, "", dummy, hiddenNodes, childrenMeshes));
