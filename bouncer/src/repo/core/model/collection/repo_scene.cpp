40a759e479c9d770cb345ef31736b59b6bc3e7f9
carmenfan <wind5320@hotmail.com>
2018-09-18 14:41:20 +0100

    ISSUE #262 stop validating the scene - it takes forever on large models!

--- bouncer/src/repo/core/model/collection/repo_scene.cpp
+++ bouncer/src/repo/core/model/collection/repo_scene.cpp
@@ -1549,7 +1549,7 @@ void RepoScene::populateAndUpdate(
 	addNodeToScene(gType, references, errMsg, &(instance.references));
 	addNodeToScene(gType, unknowns, errMsg, &(instance.unknowns));
 
-	validateScene();
+	//validateScene();
 }
 
 void RepoScene::validateScene()
