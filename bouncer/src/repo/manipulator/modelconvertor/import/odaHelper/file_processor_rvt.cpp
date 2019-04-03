e2a4052e90723067dea95837a9b811ea60b770c7
carmenfan <wind5320@hotmail.com>
2019-04-03 12:04:18 +0100

    ISSUE #317 fix test

--- bouncer/src/repo/manipulator/modelconvertor/import/odaHelper/file_processor_rvt.cpp
+++ bouncer/src/repo/manipulator/modelconvertor/import/odaHelper/file_processor_rvt.cpp
@@ -97,7 +97,7 @@ OdString Get3DLayout(OdDbBaseDatabasePEPtr baseDatabase, OdBmDatabasePtr bimData
 			//set first 3D view available
 			if (layoutName.isEmpty()) {
 				layoutName = pLayout->name(layouts->object());
-				repoInfo << "First layout name is " << layoutName;
+				repoInfo << "First layout name is " << convertToStdString(pDBDrawing->getName());
 			}
 
 			//3D View called "3D" has precedence
