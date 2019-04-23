305b0ea6a00a81d5625af998445f386e9d690e76
carmenfan <wind5320@hotmail.com>
2019-04-23 12:34:57 +0100

    ISSUE #320 add file manager ut

---
+++ test/src/unit/repo/core/handler/fileservice/ut_repo_file_manager.cpp
@@ -0,0 +1,32 @@
+/**
+*  Copyright (C) 2019 3D Repo Ltd
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as
+*  published by the Free Software Foundation, either version 3 of the
+*  License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtest/gtest.h>
+#include <repo/core/handler/fileservice/repo_file_manager.h>
+#include <repo/core/model/bson/repo_node.h>
+#include <repo/lib/repo_exception.h>
+#include <repo/lib/repo_utils.h>
+#include "../../../../repo_test_database_info.h"
+
+using namespace repo::core::handler::fileservice;
+
+TEST(FileManager, GetManager)
+{
+	EXPECT_THROW(FileManager::getManager(), repo::lib::RepoException);
+
+
+}
