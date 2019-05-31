7b180f59abde5b264ce680fbe9e5da4cbf64acd9
carmenfan <wind5320@hotmail.com>
2019-05-31 18:10:05 +0100

    ISSUE #330 update model IDs

--- tools/bouncer_worker/importToy.js
+++ tools/bouncer_worker/importToy.js
@@ -203,9 +203,9 @@ module.exports = function(dbConfig, modelDir, username, database, project, skipP
 	function renameGroups(db){
 
 		const subModelNameToOldID = {
-			"Lego_House_Architecture" : "33586989-6130-4787-8ea5-b56b81286ccf",
-			"Lego_House_Landscape" : "81abd908-d0b2-46f5-a9d5-38471dbfab72",
-			"Lego_House_Structure" : "94020bb8-07d3-4811-ae29-040c961ed92f"
+			"Lego_House_Architecture" : "a29d06a0-51b7-45d7-9d33-41a62e036e5b",
+			"Lego_House_Landscape" : "76fa299d-b626-48c5-9327-05fa371b3a49",
+			"Lego_House_Structure" : "7ea2eb1f-3ba6-4f13-b6e5-a1b53f17d0c6"
 		};
 
 		return new Promise((resolve, reject) => {
