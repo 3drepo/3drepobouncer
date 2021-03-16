d60fbab61d8471e46881dc9ba6b6b72219cec4fe
Keif Gwinn <keif.gwinn@3drepo.org>
2021-03-16 17:01:16 +0000

    #400 different method for working out max memory

--- tools/bouncer_worker/src/lib/processMonitor.js
+++ tools/bouncer_worker/src/lib/processMonitor.js
@@ -9,13 +9,17 @@ processMonitor.logLabel = { label: 'PROCESSMON' };
 
 let pidArray = [];
 let stats = [];
+let maxMemory = 0;
+let startMemory = 0;
 
 processMonitor.maxmem = async () => {
 	try {
 		// logger.debug(("[processMonitor]:maxmem: " + Date.now(),"pidArray",pidArray),processMonitor.logLabel)
+		let data = Number(fs.readFileSync('/sys/fs/cgroup/memory/memory.usage_in_bytes'))
 		if (pidArray.length > 0) {
 			stats = await pidusage(pidArray);
-			console.log(stats)
+			if (data > maxMemory) maxMemory = data
+			// console.log(stats)
 			// logger.debug(stats,processMonitor.logLabel)
 		}
 	} catch (err) { logger.verbose(`[maxmem][error]: ${err}`, processMonitor.logLabel); }
@@ -38,6 +42,7 @@ processMonitor.monitor = async () => {
 
 processMonitor.startMonitor = async (inputPID) => {
 	pidArray.push(inputPID);
+	startMemory = Number(fs.readFileSync('/sys/fs/cgroup/memory/memory.usage_in_bytes'))
 	processMonitor.monitor();
 	logger.verbose(`[${inputPID}]: a startMonitor event occurred!`, processMonitor.logLabel);
 };
@@ -48,10 +53,12 @@ processMonitor.stopMonitor = async (inputPID) => {
 	if (index !== -1) {
 		pidArray.splice(index, 1);
 	}
-	processMonitor.maxMemory = stats[inputPID].memory;
+	processMonitor.maxMemory = maxMemory - startMemory; // stats[inputPID].memory;
 	processMonitor.processTime = stats[inputPID].elapsed;
 	logger.verbose(`[${inputPID}]: a stopMonitor event occurred! elapsed: ${processMonitor.processTime}`, processMonitor.logLabel);
 	// stats = [];
+	startMemory = 0 
+	maxMemory = 0
 };
 
 module.exports = processMonitor;
