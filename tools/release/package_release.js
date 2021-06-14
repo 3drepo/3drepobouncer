811c152dc17bcfa13b1c8002d75706be6bd92e09
Carmen Fan <wind5320@hotmail.com>
2021-06-14 12:39:37 +0100

    ISSUE #473 use packaged version in travis

---
+++ tools/release/package_release.js
@@ -0,0 +1,126 @@
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * ============== Constant Declarations - edit as necessary ==============
+ */
+const verbose = true;
+const exts = [".tx", ".txv", ".dll", ".so"];
+
+const externalLibraries = [
+	{
+		rootEnvVar: "MONGO_ROOT",
+		subPath: ["lib"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "ASSIMP_ROOT",
+		subPath: ["bin", "lib"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "ODA_ROOT",
+		subPath: ["exe/vc14_amd64dll", "bin/lnxX64_8.3dll"],
+		subFolder:  ["exe/vc14_amd64dll/CSV", "bin/lnxX64_8.3dll/CSV"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "THRIFT_ROOT",
+		subPath: ["lib"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "OCCT_ROOT",
+		subPath: ["lib", "exe/vc14_amd64dll", "bin/lnxX64_8.3dll"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "IFCOPENSHELL_ROOT",
+		subPath: ["lib"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "SYNCHRO_READER_ROOT",
+		subPath: ["lib"],
+		subFolder: ["../plugins"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "BOOST_ROOT",
+		subPath: ["lib", "lib64-msvc-14.0"],
+		extensions: exts
+	},
+	{
+		rootEnvVar: "BOUNCER_ROOT",
+		subPath: ["lib", "bin"],
+		extensions: [...exts, ".exe"]
+	}
+];
+
+/**
+ * =======================================================================
+ */
+
+// https://stackoverflow.com/a/64255382
+const copyDir = (src, dest) => {
+	verbose && console.log(`Copying folder ${src} to ${dest}...`);
+    fs.mkdirSync(dest, { recursive: true });
+    const entries = fs.readdirSync(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        const srcPath = path.join(src, entry.name);
+        const destPath = path.join(dest, entry.name);
+
+        entry.isDirectory() ?
+            copyDir(srcPath, destPath) :
+            fs.copyFileSync(srcPath, destPath);
+    }
+};
+
+const folderPath = `3drepobouncer`
+
+fs.mkdirSync(folderPath);
+
+externalLibraries.forEach(({rootEnvVar, subPath, subFolder, extensions}) => {
+	verbose && console.log(`Processing ${rootEnvVar}...`);
+	const rootPath = process.env[rootEnvVar];
+	verbose && console.log(`\tPath is: ${rootPath}...`);
+	if(!( rootPath && fs.existsSync(rootPath))) {
+		console.log(`\tCould not find env var ${rootEnvVar}. Skipping...`);
+		return;
+	}
+
+	if(subPath && subPath.length) {
+		subPath.forEach((subDir) => {
+			const fullPath = path.join(rootPath, subDir);
+			verbose && console.log(`\tSub dir: ${subDir}... (${fullPath})`);
+			if(fs.existsSync(fullPath)) {
+				const files = fs.readdirSync(fullPath);
+				files.forEach((file) => {
+					const fileExt = path.extname(file);
+					if (extensions.includes(fileExt)) {
+						const filePath = path.join(fullPath, file);
+						verbose && console.log(`\tCopying file: ${filePath}...`);
+						const destPath = path.join(folderPath, file);
+						fs.copyFileSync(filePath, destPath);
+					}
+				});
+			} else {
+				verbose && console.log(`\t${subDir} does not exist. skipping...`);
+			}
+		});
+	}
+
+	if(subFolder && subFolder.length) {
+		subFolder.forEach((subDir) => {
+			const fullPath = path.join(rootPath, subDir);
+			verbose && console.log(`\tFound sub folder: ${fullPath}`);
+			if(fs.existsSync(fullPath)) {
+				copyDir(fullPath, path.join(folderPath, path.basename(subDir)));
+			}
+		});
+	}
+
+
+});
+
