9e8455e40d65e01c7ab2fdcc63a8e642581229bf
Carmen Fan <wind5320@hotmail.com>
2021-06-23 11:54:20 +0100

    Issue 473 - Deployment improvements (#474)
    * ISSUE #473 add kill switch
    
    * ISSUE #473 ability to set VALID_UNTIL through cmake
    
    * ISSUE #473 use packaged version in travis
    
    * ISSUE #473 fix envvar typo
    
    * ISSUE #473 add empty string as an acceptable extension
    
    * ISSUE #473 set LD_LIBRARY_PATH
    
    * ISSUE #473 add more extensions
    
    * ISSUE #473 the .7 files are actually OCCT files...
    
    * ISSUE #473 fix plugin path
    
    * ISSUE #473 check ldd of synchro plugin
    
    * ISSUE #473 pull in thrift
    
    * ISSUE #473 reenable test
    
    * ISSUE #473 include txt files from oda bin folder
    
    * ISSUE #473 remove extra i
    
    * ISSUE #473 add optional parameter to take in destination path
    
    * ISSUE #473 BOUNCER_ROOT should be REPOBOUNCER_ROOT
    
    * ISSUE #473 add license info dump when version command is called

--- bouncer/src/repo/repo_controller.cpp
+++ bouncer/src/repo/repo_controller.cpp
@@ -16,6 +16,9 @@
 */
 
 #include "repo_controller_internal.cpp.inl" //Inner class implementation
+#include <ctime>
+#include <boost/date_time.hpp>
+#include "lib/repo_exception.h"
 
 using namespace repo;
 
@@ -24,6 +27,14 @@ RepoController::RepoController(
 	const uint32_t &numConcurrentOps,
 	const uint32_t &numDbConn)
 {
+#ifdef VALID_UNTIL
+
+	std::time_t currentTS = std::time(nullptr);
+	if (currentTS > VALID_UNTIL) {
+		throw repo::lib::RepoValidityExpiredException();
+	}
+#endif
+
 	//RepoController follows the Pimpl idiom http://www.gotw.ca/gotw/028.htm
 	//This is done to avoid high dependencies on other headers for library users
 	//Actual implementations are in _RepoControllerImpl
@@ -420,4 +431,18 @@ bool RepoController::isVREnabled(const RepoController::RepoToken *token,
 std::string RepoController::getVersion()
 {
 	return impl->getVersion();
+}
+
+std::string RepoController::getLicenseInfo()
+{
+	std::stringstream ss;
+	ss << "License is valid until: ";
+#ifdef VALID_UNTIL
+	int64_t validity = VALID_UNTIL;
+	auto timeUntil = boost::posix_time::from_time_t(validity);
+	ss << boost::posix_time::to_iso_extended_string(timeUntil) << " (timestamp in seconds: " << validity << ")";
+#else
+	ss << "Forever";
+#endif
+	return ss.str();
 }
