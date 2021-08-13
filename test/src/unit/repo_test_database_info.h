b76f151f6b60963488c055d4a56e2178b2dbe3ac
Carmen Fan <wind5320@hotmail.com>
2021-08-13 16:00:05 +0100

    ISSUE #480 changes for synchro 6.4 (#488)
    * ISSUE #480 changes for synchro 6.4
    
    * ISSUE #480 use 6.4 on unit test

--- test/src/unit/repo_test_database_info.h
+++ test/src/unit/repo_test_database_info.h
@@ -57,6 +57,7 @@ const static std::string synchroFile = "synchro.spm";
 const static std::string synchroWithTransform = "withTransformations.spm";
 const static std::string synchroOldVersion = "synchro6_1.spm";
 const static std::string synchroVersion6_3 = "synchro6_3.spm";
+const static std::string synchroVersion6_4 = "synchro6_4.spm";
 
 const static std::string emptyFile = "empty.json";
 const static std::string emptyJSONFile = "empty2.json";
