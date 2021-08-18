347a3804d19bff730a37270342bf17d7abb9d632
har00n-haider <72197495+har00n-haider@users.noreply.github.com>
2021-08-18 15:21:44 +0100

    ISSUE #485 Absorb the bouncer wrapper code and add option to build alongside bouncer (#486)
    * ISSUE #485 adding the wrapper files to the repo
    
    * ISSUE #485 Adding the wrapper subdirectory and option to top level cmake
    
    * ISSUE #485 updating python script to create src level sources for wrapper
    
    * ISSUE #485 running the python script for the wrapper folder
    
    * ISSUE #485 removing unrequired code from the top level wrapper CMakeLists
    
    * ISSUE #485 deleting the local modules folder for the wrapper
    
    * ISSUE #485 adding the preprocesser definition back into the top level CMakeLists
    
    * ISSUE #485 removing readme (moved to wiki)
    
    * ISSUE #485 updating the wrapper to take the version from the core dll only
    
    * ISSUE #485 adding the wrapper to the travis build script
    
    * ISSUE #485 adding wrapper test project to cmake
    
    * ISSUE #485 amendend the wrapper test cmakelists to remove API exporting and linking correctly to libraries
    
    * ISSUE #485 splitting the definition/declaration for the csharp interface so the dllexport/dllimport work correctly for the same header file
    
    * ISSUE #485 adding dependency option for wrapper_tests on wrapper. Splitting out the build of gtest framework.
    
    * ISSUE #485 adding unit test for the export function of the wrapper dll
    
    * ISSUE #485 switching on the wrapper test exe build on travis
    
    * ISSUE #485 updating comment in cmake
    
    * ISSUE #485 moving the gtest build before test files so the include directories are populated
    
    * ISSUE #485 adding a command to run the tests to the travis build system
    
    * ISSUE #485 switched to use a readonly for test stability
    
    * ISSUE #485 moving internal utility function to cpp
    
    * ISSUE #485 removing export macro from function definitions
    
    * ISSUE #485 updating copyright and adding test macro to boolean
    
    * ISSUE #485 removing extern C linkage from the function definitions
    
    * ISSUE #485 updating the test to not write to the readonly database
    
    * ISSUE #485 forcing head revision on the tests
    
    * ISSUE #485 remove double license header
    
    * ISSUE #485 wrapper should be built by default
    
    Co-authored-by: Carmen Fan <wind5320@hotmail.com>

---
+++ wrapper_test/src/unit/ut_repo_csharp_interface.cpp
@@ -0,0 +1,92 @@
+/**
+*  Copyright (C) 2021 3D Repo Ltd
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as
+*  published by the Free Software Foundation, either version 3 of the
+*  License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include "repo_csharp_interface.h"
+#include <boost/filesystem.hpp>
+
+static std::string getDataPath(
+	const std::string& file)
+{
+	char* pathChr = getenv("REPO_MODEL_PATH");
+	std::string returnPath = "";
+
+	if (pathChr)
+	{
+		std::string path = pathChr;
+		path.erase(std::remove(path.begin(), path.end(), '"'), path.end());
+		boost::filesystem::path fileDir(path);
+		auto fullPath = fileDir / boost::filesystem::path(file);
+		returnPath = fullPath.string();
+	}
+	return returnPath;
+}
+
+TEST(RepoCsharpInterface, LoadScene)
+{
+	// using the readly ony database so the project should be in 
+	// the test db, even if a round of tests has already been run on it
+	std::string database = "sampleDataReadOnly";
+	std::string project = "3drepoBIM";
+	std::string revisionID = "";
+	std::string configPath = getDataPath("config/config.json");
+	bool connected = repoConnect(&configPath[0]);
+	// check connection
+	ASSERT_TRUE(connected);
+	bool loadedScene = repoLoadSceneForAssetBundleGeneration(
+		&database[0],
+		&project[0],
+		&revisionID[0]);
+	// check scene loaded
+	ASSERT_TRUE(loadedScene);
+	// vr enabled check
+	ASSERT_FALSE(repoIsVREnabled());
+	// super mesh count
+	int numSuperMeshes = repoGetNumSuperMeshes();
+	ASSERT_EQ(4, numSuperMeshes);
+	// federation check
+	bool isFederation = repoIsFederation(
+		&database[0],
+		&project[0]);
+	ASSERT_FALSE(isFederation);
+}
+
+TEST(RepoCsharpInterface, AssetBundleSave)
+{
+	// using the read/write database
+	std::string database = "stUpload";
+	std::string project = "cube";
+	std::string revisionID = "";
+	std::string configPath = getDataPath("config/config.json");
+	bool connected = repoConnect(&configPath[0]);
+	// check connection
+	ASSERT_TRUE(connected);
+	// load scene
+	bool loadedScene = repoLoadSceneForAssetBundleGeneration(
+		&database[0],
+		&project[0],
+		&revisionID[0]);
+	// check scene loaded
+	ASSERT_TRUE(loadedScene);
+	// asset bundle save check
+	std::string dummyAssetBundlePath = getDataPath(
+		"textures/brick_non_uniform_running_burgundy.png");
+	char* dummyAssetBundlePathPtr = &dummyAssetBundlePath[0];
+	bool assetsSavedCorrectly = repoSaveAssetBundles(&dummyAssetBundlePathPtr, 1);
+	ASSERT_TRUE(assetsSavedCorrectly);
+}
