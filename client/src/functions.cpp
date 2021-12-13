d4dae10c97dfdff11c0d212ba924cb4b439fce44
haroon-haider <haroon.haider@3drepo.org>
2021-12-13 23:54:53 +0900

    ISSUE #519 removed remaining refernce to licnse info function

--- client/src/functions.cpp
+++ client/src/functions.cpp
@@ -135,7 +135,6 @@ int32_t performOperation(
 	else if (command.command == cmdVersion || command.command == cmdVersion2)
 	{
 		std::cout << "3D Repo Bouncer Client v" + controller->getVersion() << std::endl;
-		std::cout << controller->getLicenseInfo() << std::endl;
 		errCode = REPOERR_OK;
 	}
 	else
