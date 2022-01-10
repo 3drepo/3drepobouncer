1626d03554324bcefe2000628181046c5df51773
Keif Gwinn <keif.gwinn@3drepo.org>
2022-01-10 15:06:12 +0000

    #523 add .2 to the release bundle

--- tools/release/package_release.js
+++ tools/release/package_release.js
@@ -22,7 +22,7 @@ const externalLibraries = [
 		rootEnvVar: "ODA_ROOT",
 		subPath: ["exe/vc14_amd64dll", "bin/lnxX64_8.3dll"],
 		subFolder:  ["exe/vc14_amd64dll/CSV", "bin/lnxX64_8.3dll/CSV"],
-		extensions: [...exts, ".txt"]
+		extensions: [...exts, ".txt", ".2"]
 	},
 	{
 		rootEnvVar: "THRIFT_ROOT",
