f5880ce096b4d43929d6b9a2e1c7d5f8c248d9af
Carmen Fan <wind5320@hotmail.com>
2022-01-17 18:15:12 +0000

    ISSUE #519 pass down license to 3drepobouncerClient when running

--- tools/bouncer_worker/src/tasks/bouncerClient.js
+++ tools/bouncer_worker/src/tasks/bouncerClient.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2020 3D Repo Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 const path = require('path');
 const logger = require('../lib/logger');
 const { config, configPath } = require('../lib/config');
@@ -15,6 +32,10 @@ const setBouncerEnvars = (logDir) => {
 
 	if (logDir) {
 		process.env.REPO_LOG_DIR = logDir;
+	}
+
+	if (config.repoLicense) {
+		process.env.REPO_LICENSE = config.repoLicense;
 	}
 };
 
