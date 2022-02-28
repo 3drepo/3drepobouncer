d42c64253e16569fb5c836e0c7d1b62b79795e4f
Carmen Fan <wind5320@hotmail.com>
2022-02-28 16:19:38 +0000

    Version 4.22.0

--- bouncer/src/repo/repo_bouncer_global.h
+++ bouncer/src/repo/repo_bouncer_global.h
@@ -35,6 +35,6 @@
 //------------------------------------------------------------------------------
 #define BOUNCER_VMAJOR 4
 
-#define BOUNCER_VMINOR "21_2"
+#define BOUNCER_VMINOR "22_0"
 #define REPO_MAX_OBJ_SIZE (16 * 1024 * 1024)
 //
