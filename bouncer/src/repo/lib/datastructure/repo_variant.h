d82e3b855ee3a13d44d411eb9b6cd9d17b3172a4
Saurabh Banethia <90389428+sbanethia@users.noreply.github.com>
2024-03-18 18:59:16 +0530

    ISSUE #462 store metadata as their original type instead of casting it all to string, and guess-cast it back
    Making the changes in the code as per the review given.

--- bouncer/src/repo/lib/datastructure/repo_variant.h
+++ bouncer/src/repo/lib/datastructure/repo_variant.h
@@ -7,8 +7,8 @@
 
 namespace repo {
     namespace lib {
-        using boostVariantType = boost::variant<int, double, std::string, bool, uint64_t, float, long,unsigned long>;
-        enum RepoDataType { INT, DOUBLE,STRING, BOOL,UINT64, FLOAT,LONG,UNSIGNEDLONG,OTHER };
+        using boostVariantType = boost::variant<int, double, std::string, bool, uint64_t, float, long>;
+        enum RepoDataType { INT, DOUBLE,STRING, BOOL,UINT64, FLOAT,LONG,OTHER };
         class RepoVariant : private boostVariantType {
         public:
             using boostVariantType::operator=;
@@ -76,10 +76,6 @@ namespace repo {
                     }
                     case repo::lib::RepoDataType::LONG: {
                         t = boost::get<long>(*this);
-                        break;
-                    }
-                    case repo::lib::RepoDataType::UNSIGNEDLONG: {
-                        t = boost::get<unsigned long>(*this);
                         break;
                     }
                     default: {
