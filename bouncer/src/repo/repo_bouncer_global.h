791201446ad261f49f83ee6a883c7d783455de50
Carmen Fan <wind5320@hotmail.com>
2024-09-04 15:10:35 +0100

    Version 5.11.3

--- bouncer/src/repo/repo_bouncer_global.h
+++ bouncer/src/repo/repo_bouncer_global.h
@@ -35,7 +35,7 @@
 //------------------------------------------------------------------------------
 #define BOUNCER_VMAJOR 5
 
-#define BOUNCER_VMINOR "11_2"
+#define BOUNCER_VMINOR "11_3"
 #define REPO_MAX_OBJ_SIZE (16 * 1024 * 1024)
 
 //
