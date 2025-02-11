46a2a6373d18442b09d99b78d1201228defc31a3
Charence <1438332+Charence@users.noreply.github.com>
2025-02-11 14:10:10 +0000

    ISSUE #737 - amend elastic config def and example

--- tools/bouncer_worker/src/lib/config.js
+++ tools/bouncer_worker/src/lib/config.js
@@ -57,12 +57,10 @@ const bouncer = object({
 });
 
 const elastic = object({
-	cloud: object({id: string()}).default(undefined),
-	auth: object({apiKey: string()}).default(undefined),
-	namespace: string(),
-	reload_connections: boolean(),
-	maxRetries: number(),
-	request_timeout: number(),
+	cloud: object({id: string().required()}),
+	auth: object({apiKey: string().required()}),
+	namespace: string().required(),
+	maxRetries: number().default(5),
 }).default(undefined);
 
 const logging = object({
