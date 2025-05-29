bf81e47c0da75203da7b0864c7e509ed4ce6c7fe
Felix Thiel <fthiel@asite.com>
2025-05-29 16:57:16 +0100

    Issue #764 changed old style method signature in the part of the synchro importer active when Synchro is disabled

--- bouncer/src/repo/manipulator/modelconvertor/import/repo_model_import_synchro.h
+++ bouncer/src/repo/manipulator/modelconvertor/import/repo_model_import_synchro.h
@@ -187,23 +187,17 @@ namespace repo {
 
 				std::string orgFile;
 #else
-				/**
-				* Generates a repo scene graph
-				* an internal representation(aiscene) needs to have
-				* been created before this call
-				* @return returns a populated RepoScene upon success.
-				*/
-				repo::core::model::RepoScene* generateRepoScene(uint8_t &errMsg) { return nullptr; }
 
 				/**
 				* Import model from a given file
 				* @param path to the file
+				* @param database handler
 				* @param error message if failed
-				* @return returns true upon success
+				* @return returns a populated RepoScene upon success.
 				*/
-				bool importModel(std::string filePath, uint8_t &errMsg) {
+				repo::core::model::RepoScene* importModel(std::string filePath, std::shared_ptr<repo::core::handler::AbstractDatabaseHandler> handler, uint8_t& errMsg) {
 					errMsg = REPOERR_SYNCHRO_UNAVAILABLE;
-					return false;
+					return nullptr;
 				}
 #endif
 			};
