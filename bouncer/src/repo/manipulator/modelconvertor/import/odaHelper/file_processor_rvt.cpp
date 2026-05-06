dfe2a20e8c596565e045f15d809e168c36c30708
Felix Thiel <fthiel@asite.com>
2026-05-06 12:03:17 +0100

    Issue #863 Fixed wrong method call causing us to use a non-inverted model to world matrix

--- bouncer/src/repo/manipulator/modelconvertor/import/odaHelper/file_processor_rvt.cpp
+++ bouncer/src/repo/manipulator/modelconvertor/import/odaHelper/file_processor_rvt.cpp
@@ -415,7 +415,7 @@ OdGeMatrix3d getModelToWorldMatrix(OdBmDatabasePtr pDb)
 
 				OdBmGeoLocationPtr pActiveLocation = OdBmGeoLocation::getActiveLocationId(pThis->database()).safeOpenObject();
 				OdGeMatrix3d activeTransform = pActiveLocation->getTransform();
-				activeTransform.inverse();
+				activeTransform.invert();
 
 				return activeTransform;
 			}
